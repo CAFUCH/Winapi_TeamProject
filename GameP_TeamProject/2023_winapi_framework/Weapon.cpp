8cdad1f653ab9d3aab281f564341ed6c748af42d
CAFUCH <tjfdk6201@gmail.com>
2023-11-24 12:03:52 +0900

    Weapon
    자동 조준을 위한 CurScene Enemy

--- GameP_TeamProject/2023_winapi_framework/Weapon.cpp
+++ GameP_TeamProject/2023_winapi_framework/Weapon.cpp
@@ -1,11 +1,19 @@
 #include "pch.h"
 #include "Weapon.h"
 
+#include "SceneMgr.h"
+
 Weapon::Weapon()
 	: m_pTex(nullptr)
 {
+	m_pCurScene = SceneMgr::GetInst()->GetCurScene();
 }
 
 Weapon::~Weapon()
 {
+}
+
+void Weapon::Update()
+{
+	//m_pCurScene.
 }
