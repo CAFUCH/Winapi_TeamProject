ff46d2ef758cea173926af5429646dbb68846972
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-12-15 11:07:28 +0900

    wer

--- GameP_TeamProject/2023_winapi_framework/Knife.cpp
+++ GameP_TeamProject/2023_winapi_framework/Knife.cpp
@@ -5,6 +5,8 @@
 #include "SceneMgr.h"
 #include "TimeMgr.h"
 
+#include "ElementMgr.h"
+
 #include "Object.h"
 #include "Texture.h"
 #include "Collider.h"
@@ -15,18 +17,21 @@ Knife::Knife()
 {
 	// 이미지 불러오기
 	m_pTex = ResMgr::GetInst()->TexLoad(L"Weapon_Knife", L"Texture\\Knife.bmp");
-
-	CreateAnimator();
-
-	GetAnimator()->CreateAnim(L"Knife", m_pTex, Vec2(0.f, 0.f),
-		Vec2(64.f, 64.f), Vec2(1.f, 0.f), 1, 1.f);
 	// 콜라이더 생성
 	CreateCollider();
 	// 콜라이더 사이즈 초기화
 	GetCollider()->SetScale(Vec2(50.f, 50.f));
 
+	CreateAnimator();
+	GetAnimator()->CreateAnim(L"Knife", m_pTex, Vec2(0.f, 0.f),
+		Vec2(64.f, 64.f), Vec2(1.f, 0.f), 1, 1.f);
+
 	m_fDistance = 250.f;
 	m_fDelay = 0.3f;
+	m_fAtkTime = 1.f;
+	m_fCurTime = 0.f;
+	m_vDir = Vec2(0, 0);
+	m_IsAtking = false;
 }
 
 Knife::~Knife()
@@ -36,17 +41,70 @@ Knife::~Knife()
 void Knife::Update()
 {
 	GetAnimator()->PlayAnim(L"Knife", true);
-	SetPos({ m_pOwner->GetPos().x - 100, m_pOwner->GetPos().y });
+	Vec2 pPos = GetPos();
+
+
+	if (m_IsAtking) {
+
+		m_fCurTime += fDT;
+
+		if (m_fCurTime >= m_fAtkTime) {
+			m_fCurTime = 0.f;
+			m_IsAtking = false;
+		}
+		else {
+			if ((float)pPos.Distance(pPos, m_vDir) > .05f) {
+
+				Vec2 dir = (m_vDir - pPos).Normalize();
+				pPos.x += dir.x *  m_fCurTime;
+				pPos.y += dir.y * m_fCurTime;
+
+				SetPos(pPos);
+			}
+		}
+	}
+	else
+	{
+		SetPos({ m_pOwner->GetPos().x + 30, m_pOwner->GetPos().y });
+	}
 }
+
+void Knife::EnterCollision(Collider* _pOther)
+{
+	if (m_IsAtking == true)
+	{
+		if (_pOther->GetObj()->GetName() == L"Enemy")
+		{
+			// 상성 효과
+			m_fDamage = ElementMgr::GetInst()->Elemenet
+			(this->m_eElement, _pOther->GetObj()->m_eElement, m_fDamage);
+
+			// 데미지 주기
+			_pOther->GetObj()->SetDamage(m_fDamage);
+
+			// 파티클 생성
+			CreateParticle((PARTICLE_TYPE)m_eElement);
+		}
+	}
+}
+
+void Knife::ExitCollision(Collider* _pOther)
+{
+}
+
+void Knife::StayCollision(Collider* _pOther)
+{
+}
+
 
 void Knife::Render(HDC _dc)
 {
-	TransparentBlt(_dc, (int)(m_vPos.x - m_vScale.x / 2)
+	/*TransparentBlt(_dc, (int)(m_vPos.x - m_vScale.x / 2)
 		, (int)(m_vPos.y - m_vScale.y / 2)
 		, m_pTex->GetWidth() * (m_vScale.x / 100)
 		, m_pTex->GetHeight() * (m_vScale.y / 100)
 		, m_pTex->GetDC(), 0, 0, m_pTex->GetWidth()
-		, m_pTex->GetHeight(), RGB(255, 0, 255));
+		, m_pTex->GetHeight(), RGB(255, 0, 255));*/
 
 	Component_Render(_dc);
 }
@@ -55,6 +113,8 @@ void Knife::Attack(Vec2 dir)
 {
 	GetAnimator()->PlayAnim(L"Knife_Attack", false);
 
+	m_vDir = dir;
+	m_IsAtking = true;
 	// if (공격 쿨타임이 지났다면)
 	// m_texture 이동 (공격 거리만큼 공격 속도로)
 
--- GameP_TeamProject/2023_winapi_framework/Knife.h
+++ GameP_TeamProject/2023_winapi_framework/Knife.h
@@ -2,16 +2,25 @@
 #include "Weapon.h"
 class Texture;
 class Knife :
-    public Weapon
+	public Weapon
 {
 public:
-    Knife();
-    ~Knife();
+	Knife();
+	~Knife();
 
 public:
-    void Update() override;
-    void Render(HDC _dc) override;
-
+	void Update() override;
+	void Render(HDC _dc) override;
 public:
-    virtual void Attack(Vec2 dir) override;
+	virtual void EnterCollision(Collider* _pOther) override;
+	virtual void ExitCollision(Collider* _pOther) override;
+	virtual void StayCollision(Collider* _pOther) override;
+public:
+	virtual void Attack(Vec2 dir) override;
+
+private:
+	Vec2 m_vDir;
+	bool m_IsAtking;
+	float m_fAtkTime;
+	float m_fCurTime;
 };
